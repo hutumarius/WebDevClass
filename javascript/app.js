9c10c7dba73c795af7c619230eeb71645b785750
hutumarius <hutumarius@yahoo.com>
2021-05-07 12:11:18 +0300

    updates

--- javascript/app.js
+++ javascript/app.js
@@ -7,35 +7,23 @@ let destiny = {
     adress: {
         city: 'Brussel',
         steet: 'Masui',
-        postcode: 1000,
+        postcode: 1000
     },
     
 
     
     phoneNumbers: {
         type: 'home',
-        number: '04384730484',
+        number: '04384730484'
     },
 
     children: ['Vlad', 'Eric'],
     spouse: 'Delia'
 };
 
-let output = `
-
-My name is ${destiny.firstname} ${destiny.lastname} and i am married with ${destiny.spouse}. 
+let output = `My name is ${destiny.firstname} ${destiny.lastname} and i am married with ${destiny.spouse}. 
 I am ${destiny.age} years old. 
-We have ${destiny.children.toString ()} and come one year ago to ${destiny.adress}.
-You can contact us ${destiny.phoneNumber}.
+We have ${destiny.children.toString ()} and came one year ago to ${destiny.adress}.
+You can contact us ${destiny.phoneNumbers}`
 
-`
-
-
-console.log (output)
-
-
-
-
-
-
-
+console.log(output)
