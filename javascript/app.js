7ae91a42b1c3e79144ce03df2a564464fb519f0c
hutumarius <hutumarius@yahoo.com>
2021-05-07 12:23:07 +0300

    updates

--- javascript/app.js
+++ javascript/app.js
@@ -22,7 +22,7 @@ let destiny = {
 
     future:function(){
         console.log('Hello')
-        console.log(`My name is ${destiny.firstname} ${destiny.lastname} and i am married with ${destiny.spouse}\nI am ${destiny.age} years old\n We have ${destiny.children.toString ()} and came one year ago to ${destiny.adress}\nYou can contact us ${destiny.phoneNumbers}`)
+        console.log(`My name is ${destiny.firstname} ${destiny.lastname} and i am married with ${destiny.spouse}\nI am ${destiny.age} years old\n We have ${destiny.children.toString ()} and came one year ago to ${destiny.adress}\nYou can contact us at ${destiny.phoneNumbers}`)
     }
 };
 
